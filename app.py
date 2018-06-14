1c02c86abed70ec997f0360452e65aafa70c3c5b
Lucy Wilson <lucyleleu@googlemail.com>
2018-06-14 12:50:51 +0000

    result route

--- app.py
+++ app.py
@@ -42,21 +42,29 @@ def user(username):
     
     if session["current_question"] > len(questions): 
         #finished quiz
-        if session['correct_answer_count'] < len(questions)/2:
-            low_score = True
-        else:
-            low_score = False
-        return render_template("result.html",
-        username=username,
-        low_score=low_score,
-        total_questions=len(questions),
-        user_score=session['correct_answer_count'])   
+        return redirect(url_for('result'))
+        
            
     return render_template('quiz.html',
     username=username, 
     question_number=session["current_question"], 
     question=questions[session["current_question"]]['question'],
     )
+    
+@app.route('/result', methods=['GET', 'POST']) 
+def result():
+    if request.method == "GET":
+        if session['correct_answer_count'] < len(questions)/2:
+            low_score = True
+        else:
+            low_score = False
+        return render_template("result.html",
+        username=session['username'],
+        low_score=low_score,
+        total_questions=len(questions),
+        user_score=session['correct_answer_count'])   
+        
+         
 
         
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)         
